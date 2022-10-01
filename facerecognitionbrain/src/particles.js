7620be2bc3b22cf07c5ff3db6dc4cb5946a05863
hollaberry <69259629+hollaberry@users.noreply.github.com>
2022-10-01 03:55:07 -0700

    updates to facerecognititon brain

---
+++ facerecognitionbrain/src/particles.js
@@ -0,0 +1,71 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Parti = () => {
+  const particlesInit = async (main) => {
+    
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+/*
+    const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  */
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      
+      /*loaded={particlesLoaded}*/
+      options={{
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    );
+  };
+export default Parti;
